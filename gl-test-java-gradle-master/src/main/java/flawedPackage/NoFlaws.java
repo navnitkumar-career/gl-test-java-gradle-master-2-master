5a88f53f0978c8a41ef440497cd04927ef2a6954
navnitkumar-career <navnitkumar.career@gmail.com>
2023-05-30 10:41:08 +0530

    first commit

---
+++ gl-test-java-gradle-master/src/main/java/flawedPackage/NoFlaws.java
@@ -0,0 +1,7 @@
+package flawedPackage;
+ 
+public class NoFlaws {
+    public NoFlaws() {
+        System.out.println("Hello World. I am flawless!"); 
+    }
+} //nov 5 2018 5:47pm from branch; 6:11pm on master
